66f53617a7fb10746746b389c04032043415e5c4
Highspeed1k <spencerpaulbrown@gmail.com>
2018-10-18 17:37:35 -0500

    album assignment completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -35,6 +35,13 @@ import albumData from './../data/albums';
                     <col id="song-duration-column" />
                     </colgroup>
                     <tbody>
+                    {this.state.album.songs.map( (song, index) =>(
+				                   <tr key={index}>
+				                   <td>{index+1}</td>
+				                   <td>{song.title}</td>
+				                   <td>{song.duration}</td>
+				                    </tr>
+			))}
                     </tbody>
                     </table>
                  </section>
