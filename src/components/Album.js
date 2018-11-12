a877de8fdb695f71aa67c4a5a17d8722ffd02a14
Highspeed1k <spencerpaulbrown@gmail.com>
2018-11-11 23:45:46 -0600

    Album checkpoint completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,7 +18,6 @@ import albumData from './../data/albums';
 
         render() {
           return (
-
               <section className="album">
               <section id="album-info">
                 <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
